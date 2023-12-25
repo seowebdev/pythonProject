fd6be8124149e575feb7a888a21544f404b5c751
Sajid Faridi <sajid.faridi76@gmail.com>
2023-12-25 14:12:30 -0800

    adding more comments from Sajid

--- calculator.py
+++ calculator.py
@@ -6,7 +6,7 @@ y = int(input("Enter second number: "))
 z = input("Do you want to * / + - : ")
 
 if z == "*":
-    print(x * y)
+    print(x * y) # print multiplication logic
 elif z == "/":
     print(x / y)
 elif z == "+":
@@ -14,4 +14,4 @@ elif z == "+":
 elif z == "-":
     print(x - y)
 else:
-    print("This didn't work")
+    print("This didn't work") #print if it doesnt work
